d84c1c1c413ebbcb189dfe24f485cecc4fbe7cbb
FathimaNajlak <fathnajlahabeeb@gmail.com>
2024-08-16 20:18:16 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:clone_netflix/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Netflix Clone',
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white, fontSize: 24),
+          bodyMedium: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          background: Colors.black,
+        ),
+        fontFamily: GoogleFonts.ptSans().fontFamily,
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
